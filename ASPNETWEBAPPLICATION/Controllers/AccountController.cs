a478ade1b2ed21b363e49793a20ac82c08365ab3
shaelarie <shae.larie@gmail.com>
2016-09-13 17:36:35 -0500

    Fixing dropdown formatting. need to add roles to items

--- ASPNETWEBAPPLICATION/Controllers/AccountController.cs
+++ ASPNETWEBAPPLICATION/Controllers/AccountController.cs
@@ -17,7 +17,7 @@ namespace ASPNETWEBAPPLICATION.Controllers
     {
         private ApplicationSignInManager _signInManager;
         private ApplicationUserManager _userManager;
-
+        
         public AccountController()
         {
         }
